a5c8dd4c4f5e69e6932026405c485d5681643411
minjun <hitobi1014@gmail.com>
2026-06-09 14:42:51 +0900

    feat. shadcn Button custom 분리 및 컴포넌트 설치

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,18 +3,14 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { Slot } from 'radix-ui';
 
 import { cn } from '@/lib/utils';
+import { customSize, customVariants } from '@/components/ui/shadcn-custom/button-custom.ts';
 
 const buttonVariants = cva(
   "group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
-        // custom
-        close: 'bg-gray-800 text-white rounded-md hover:bg-gray-900',
-        confirm: 'bg-emerald-800 text-white rounded-md hover:bg-emerald-900', // 승인, 확인, 배정 등 긍정 관련된 기능
-        cancel: 'bg-red-600 text-white rounded-md hover:bg-red-700', // 취소, 부정 관련된 기능
-        modify: 'bg-sky-800 text-white rounded-md hover:bg-sky-900', // 수정 관련된 기능
-        // base
+        ...customVariants,
         default: 'bg-primary text-primary-foreground hover:bg-primary/80',
         outline:
           'border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50',
@@ -27,9 +23,7 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        // custom
-        base: 'px-4 h-8',
-        // base
+        ...customSize,
         default:
           'h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2',
         xs: "h-6 gap-1 rounded-[min(var(--radius-md),10px)] px-2 text-xs in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
---
+++ src/components/ui/shadcn-custom/button-custom.ts
@@ -0,0 +1,11 @@
+export const customVariants = {
+  close: 'bg-gray-800 text-white rounded-md hover:bg-gray-900',
+  confirm: 'bg-emerald-800 text-white rounded-md hover:bg-emerald-900', // 승인, 확인, 배정 등 긍정 관련된 기능
+  cancel: 'bg-red-600 text-white rounded-md hover:bg-red-700', // 취소, 부정 관련된 기능
+  modify: 'bg-sky-800 text-white rounded-md hover:bg-sky-900', // 수정 관련된 기능
+} as const;
+
+export const customSize = {
+  // custom
+  base: 'px-4 h-8',
+} as const;
