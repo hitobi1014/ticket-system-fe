c8960d4ac6a39b2aedeb094e84c8c4557ff3241d
minjun <hitobi1014@gmail.com>
2026-06-29 10:37:48 +0900

    feat: 로그인 상태에서만 로그아웃 버튼 보이게 변경

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -8,20 +8,22 @@ interface PageHeaderProps {
 }
 
 export default function PageHeader({ title, icon }: PageHeaderProps) {
-  const { logout } = useAuthStore();
+  const { logout, isAuthenticated } = useAuthStore();
   return (
     <div className="flex justify-between bg-surface-secondary border-b border-surface-accent  py-2.5 px-4">
       <div className="flex items-center gap-x-2">
         <div className="text-content-primary">{icon}</div>
         <h2 className="text-content-primary text-lg font-medium">{title}</h2>
       </div>
-      <div
-        className="flex items-center gap-x-2 text-content-primary cursor-pointer"
-        onClick={logout}
-      >
-        <IconLogout stroke={2} />
-        로그아웃
-      </div>
+      {isAuthenticated && (
+        <div
+          className="flex items-center gap-x-2 text-content-primary cursor-pointer"
+          onClick={logout}
+        >
+          <IconLogout stroke={2} />
+          로그아웃
+        </div>
+      )}
     </div>
   );
 }
