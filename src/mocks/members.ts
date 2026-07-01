df43157488715cc8183d6a8b86797643284db1f5
minjun <hitobi1014@gmail.com>
2026-07-01 14:16:26 +0900

    fix: member mock 주석처리
    - git pr용 추가

--- src/mocks/members.ts
+++ src/mocks/members.ts
@@ -1,14 +1,14 @@
-// =====================
-// 회원 데이터 (10명)
-// =====================
-import { INSTRUMENTS, type Member } from '@/types/member.ts';
-import { faker } from '@faker-js/faker/locale/ko';
-
-export const mockMembers: Member[] = Array.from({ length: 15 }, (_, i) => ({
-  id: i + 1,
-  name: faker.person.fullName(),
-  instrument: faker.helpers.arrayElement(INSTRUMENTS),
-  point: faker.number.int({ min: 5, max: 20 }),
-  allocatedTickets: faker.number.int({ min: 0, max: 5 }),
-  color: faker.color.rgb({ format: 'hex' }),
-}));
+// // =====================
+// // 회원 데이터 (10명)
+// // =====================
+// import { INSTRUMENTS, type Member } from '@/types/member.ts';
+// import { faker } from '@faker-js/faker/locale/ko';
+//
+// export const mockMembers: Member[] = Array.from({ length: 15 }, (_, i) => ({
+//   id: i + 1,
+//   name: faker.person.fullName(),
+//   instrument: faker.helpers.arrayElement(INSTRUMENTS),
+//   point: faker.number.int({ min: 5, max: 20 }),
+//   allocatedTickets: faker.number.int({ min: 0, max: 5 }),
+//   color: faker.color.rgb({ format: 'hex' }),
+// }));
