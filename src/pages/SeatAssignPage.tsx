c8fbf5dcd99a9f8e385509b9cbb77336d6d37315
minjun <hitobi1014@gmail.com>
2026-07-02 15:54:13 +0900

    design: SeatAssignSidebar 너비 고정

--- src/pages/SeatAssignPage.tsx
+++ src/pages/SeatAssignPage.tsx
@@ -122,9 +122,9 @@ export default function SeatAssignPage() {
           </div>
         </div>
 
-        <div className="flex gap-x-4 flex-1 min-h-0">
+        <div className="flex gap-x-4 h-full">
           {/* 좌측 메인 */}
-          <div className="flex-1 min-h-0 overflow-hidden">
+          <div className="h-full overflow-hidden">
             {floors.map((floor) => (
               <SeatAssignGrid
                 key={floor.id}
